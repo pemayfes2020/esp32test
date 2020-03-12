4056b95febfe19507f8b3e12de441e8ac8cce2b6
Yutarotaro <tengyeyu@gmail.com>
2020-03-12 14:57:52 +0900

    esp32bluetooth通信お試し

---
+++ boo.cpp
@@ -0,0 +1,44 @@
+#include <chrono>
+#include <thread>
+#include <iostream>
+#include <boost/asio.hpp>
+
+#define SERIAL_PORT "/dev/cu.esp-ESP32SPP"
+
+
+auto main() -> int {
+  boost::asio::io_service io;
+
+  // ポートは /dev/ttyACM0
+  boost::asio::serial_port serial(io, SERIAL_PORT);
+
+      // 速度は 9600bps
+  serial.set_option(boost::asio::serial_port_base::baud_rate(115200));
+  
+  // テキトウに1秒待つ
+  std::this_thread::sleep_for(std::chrono::seconds(1));
+
+  // "ping" を送信
+  // boost::asio::write(serial, boost::asio::buffer("11"));
+
+  for (int i = 0; i < 2;i++){
+      // serial から response_buf に '\n' まで読み込む
+
+      boost::asio::write(serial, boost::asio::buffer("a"));
+
+
+      boost::asio::streambuf response_buf;
+      boost::asio::read_until(serial, response_buf, '0');
+
+      // boost::asio::write(serial, response_buf);
+
+
+      //**************************************************//
+
+      // boost::asio::SyncReadStream *s;
+
+    
+      // 表示して終わり
+      std::cout << boost::asio::buffer_cast<const char*>(response_buf.data());
+  }
+}
