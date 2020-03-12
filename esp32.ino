1b8570ec1d284f6210fb3ec8ee0cba9347dc06dc
Yutarotaro <tengyeyu@gmail.com>
2020-03-12 15:14:07 +0900

    add .ino files

---
+++ esp32.ino
@@ -0,0 +1,35 @@
+#include "BluetoothSerial.h"
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("esp"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+}
+
+int cnt = 0;
+
+void loop() {
+
+//  SerialBT.write(cnt);
+  SerialBT.println(cnt);       //bluetoothを通じてMacのプログラムに送られる
+  
+  //  Serial.write(cnt);
+//    Serial.println("Serial");  //ArduinoIDEのシリアルポートに出力
+
+  cnt++;
+  if (SerialBT.available()) {
+      Serial.println(SerialBT.read());
+//      SerialBT.println("Hello");
+  }
+  if (SerialBT.available()) {
+//    Serial.write(SerialBT.read());
+//    Serial.print(SerialBT.read());
+  }
+  delay(1000);
+}
